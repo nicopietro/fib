50c96fa2971a37ec46ff85050475b307221c8b1e
Nicolas Pietro <nicolas.pietro@roarinc.com>
2024-11-07 20:42:50 +0100

    Created fib.py file

---
+++ fib.py
@@ -0,0 +1,39 @@
+import argparse
+
+def fibonacci(n: int):
+    if n < 0:
+        raise ValueError("n must be greater or equal to zero.")
+    if n < 2:
+        return n
+    n0 = 0
+    n1 = 1
+    for _ in range(n - 1):
+        nth = n0 + n1
+        n0 = n1
+        n1 = nth
+    return nth
+
+
+cache = {}
+
+
+def fibonacci_recursive(n: int):
+    if n < 0:
+        raise ValueError("n must be greater or equal to zero.")
+    if n < 2:
+        return n
+    if n in cache:
+        return cache[n]
+    nth = fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+    cache[n] = nth
+    return nth
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('nth', type=int, help='Numero de repeticiones')
+
+    args = parser.parse_args()
+
+    r = fibonacci(args.nth)
+    print(r)
